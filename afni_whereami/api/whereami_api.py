e7665a1ceed72708ad66c9bf913e067ba509b490
donishadsmith <donishasmith@outlook.com>
2026-05-26 20:33:27 -0400

    Updat plus add resource links

--- afni_whereami/api/whereami_api.py
+++ afni_whereami/api/whereami_api.py
@@ -1,6 +1,12 @@
 """
-For documentation, a copy of the Python file running fastapi that is hosted on a continuously running instance
+For documentation: a copy of the Python file running fastapi that is hosted on a continuously running instance
 on Oracle Cloud. It's the free tier so the instance only has 1 GB ram and 1 OCPU.
+
+Uses systemd to create and manage a process that runs uvicorn in the background so it can listen to incoming
+requests continuously on the cloud vm:
+https://www.freedesktop.org/software/systemd/man/latest/systemd.service.html?__goaway_challenge=meta-refresh&__goaway_id=8887fcae4814db9e37888fc15975b6b4&__goaway_referer=https%3A%2F%2Fwww.google.com%2F
+
+https://github.com/fastapi/fastapi/discussions/14783
 """
 
 import subprocess
@@ -12,7 +18,7 @@ app = FastAPI()
 @app.post("/")
 def whereami(request_data: dict):
     cmd = (
-        f"podman exec afni whereami "
+        f"podman run localhost/afni whereami "
         f"{request_data['x']} {request_data['y']} {request_data['z']} "
         f"-lpi -space MNI_2009c_asym -atlas FS.afni.MNI2009c_asym -atlas Brodmann_Pijn_AFNI"
     )
