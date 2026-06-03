d2b61414de8daa036450efdad3b83dca1ffb891c
donishadsmith <donishasmith@outlook.com>
2026-06-03 00:47:14 -0400

    Update

--- bids_conversion_pipeline/bids_conversion.py
+++ bids_conversion_pipeline/bids_conversion.py
@@ -4,11 +4,16 @@ from typing import Literal, Optional
 
 sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
-import nibabel as nib
 from nibabel.filebasedimages import ImageFileError
 
 from bidsaid._decorators import check_nifti
-from bidsaid.io import _copy_file, load_nifti, compress_image, regex_glob
+from bidsaid.io import (
+    _copy_file,
+    load_nifti,
+    compress_image,
+    is_nifti_truncated,
+    regex_glob,
+)
 from bidsaid.logging import setup_logger
 from bidsaid.path_utils import is_valid_date
 
@@ -86,37 +91,14 @@ def _copy_nifti_files(nifti_file: Path, temp_dir: Path) -> None:
         return
 
     if nifti_file.name.endswith(".nii"):
-        try:
-            compress_image(dst_file, dst_file.parent, remove_src_file=True)
-        except OSError:
-            LGR.exception(
-                f"An OSError occured while compressing the following file: {dst_file}. "
-                "Attempting to compress directly with gzip.",
-                exc_info=True,
+        if is_nifti_truncated(dst_file):
+            LGR.warning(
+                f"Truncated NIfTI detected: {dst_file}. Removing from temporary directory."
             )
-            compressed_file = dst_file
-            try:
-                compressed_file = compress_image(
-                    dst_file,
-                    dst_file.parent,
-                    remove_src_file=True,
-                    use_gzip=True,
-                    return_dst_file=True,
-                )
-                # File will likely compress but nibabel needs to be able to get functional data
-                nib.load(compressed_file).get_fdata()
-            except OSError:
-                LGR.exception(
-                    f"An OSError occured while compressing the following file with gzip and loading in the file: {dst_file}. "
-                    f"Issue is likely that the file is truncated and does not match the expected filesize in the header (data at the end is missing) "
-                    "Removing file from temporary directory.",
-                    exc_info=True,
-                )
-                if compressed_file.exists():
-                    compressed_file.unlink()
-
-                if dst_file.exists():
-                    dst_file.unlink()
+            if dst_file.exists():
+                dst_file.unlink()
+        else:
+            compress_image(dst_file, dst_file.parent, remove_src_file=True)
 
 
 @check_nifti(nifti_param_name="nifti_file")
